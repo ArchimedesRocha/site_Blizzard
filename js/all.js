bf68e6ad7220514097f795ceb9cfa95d17611377
Archimedes rocha <dev.archimedesrocha@gmail.com>
2022-02-15 21:39:11 -0300

    Section Hero Done

--- js/all.js
+++ js/all.js
@@ -1,1 +1,1 @@
-"use strict";var slide_hero=new Swiper(".slide-principal",{effect:"fade"});
+"use strict";var slide_thumbnail=new Swiper(".slide-thumbnail",{slidesPerView:5,direction:"vertical",spaceBetween:20,watchSlidesProgress:!0}),slide_hero=new Swiper(".slide-principal",{effect:"fade",thumbs:{swiper:slide_thumbnail},autoplay:{delay:5e3,disableOnInteraction:!1}});
--- js/scripts/main.js
+++ js/scripts/main.js
@@ -1,3 +1,17 @@
+var slide_thumbnail = new Swiper(".slide-thumbnail", {
+  slidesPerView: 5,
+  direction: "vertical",
+  spaceBetween: 20,
+  watchSlidesProgress: true,
+});
+
 var slide_hero = new Swiper(".slide-principal", {
   effect: "fade",
+  thumbs: {
+    swiper: slide_thumbnail,
+  },
+  autoplay: {
+    delay: 5000,
+    disableOnInteraction: false,
+  },
 });
