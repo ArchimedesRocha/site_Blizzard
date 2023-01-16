bf8396ebffb63948be5261cc23632424e0eae550
Archimedes Rocha <dev.archimedesrocha@gmail.com>
2023-01-16 00:26:30 -0300

    Done Site Blizzard

--- js/scripts/main.js
+++ js/scripts/main.js
@@ -6,7 +6,21 @@ var slide_thumbnail = new Swiper(".slide-thumbnail", {
   direction: "vertical",
   spaceBetween: 20,
   watchSlidesProgress: true,
+  breakpoints: {
+    320: {
+      direction: 'horizontal',
+    },
+    1150: {
+      direction: 'vertical',
+    }
+  }
 });
+
+//--------------------------------------------------------------------------------------
+//PROGRESS BAR DENTRO DE UM SLIDE COM SWIPER
+//--------------------------------------------------------------------------------------
+
+const progressSlide = document.querySelector('.js-progress')
 
 var slide_hero = new Swiper(".slide-principal", {
   effect: "fade",
@@ -17,6 +31,23 @@ var slide_hero = new Swiper(".slide-principal", {
     delay: 5000,
     disableOnInteraction: false,
   },
+  on: {
+    init: function() {
+      progressSlide.classList.remove('animate')
+      progressSlide.classList.remove('active')
+      progressSlide.classList.add('animate')
+      progressSlide.classList.add('active')
+    },
+    slideChangeTransitionStart: function() {
+      progressSlide.classList.remove('animate')
+      progressSlide.classList.remove('active')
+      progressSlide.classList.add('active')
+
+    },
+    slideChangeTransitionEnd: function() {
+      progressSlide.classList.add('animate')
+    }
+  }
 });
 
 //--------------------------------------------------------------------------------------
@@ -68,13 +99,47 @@ btnClosedModal.addEventListener("click", () => {
 });
 
 //--------------------------------------------------------------------------------------
+//MODAL MOBILE
+//--------------------------------------------------------------------------------------
+const btnOpenModalMobileMenu = document.querySelector(".js-open-modal-mobile-menu");
+const btnClosedModalMobileMenu = document.querySelector(".js-closed-modal-mobile-menu");
+
+btnOpenModalMobileMenu.addEventListener("click", (event) => {
+  event.preventDefault();
+  let tagHtml = document.documentElement;
+  tagHtml.classList.add("show-modal-mobile-menu");
+});
+btnClosedModalMobileMenu.addEventListener("click", (event) => {
+  event.preventDefault();
+  let tagHtml = document.documentElement;
+  tagHtml.classList.remove("show-modal-mobile-menu");
+});
+
+//--------------------------------------------------------------------------------------
+//MODAL LOGIN MOBILE
+//--------------------------------------------------------------------------------------
+const btnOpenLoginMobile = document.querySelector(".js-open-login-mobile");
+const btnOpenModalMobile = document.querySelector(".js-closed-login-mobile");
+
+btnOpenLoginMobile.addEventListener("click", (event) => {
+  event.preventDefault();
+  let tagHtml = document.documentElement;
+  tagHtml.classList.add("show-modal-login-mobile");
+});
+btnOpenModalMobile.addEventListener("click", () => {
+  let tagHtml = document.documentElement;
+  tagHtml.classList.remove("show-modal-login-mobile");
+});
+
+//--------------------------------------------------------------------------------------
 //NAVEGAÇÃO POR TABS COMPLETO - MENU
 //--------------------------------------------------------------------------------------
 const btnMenu = document.querySelectorAll('.js-btn-menu');
 const menuHeader = document.querySelectorAll('.js-menu');
+const closeMenu = document.querySelectorAll('.js-close-sports-menu')
 
 btnMenu.forEach((btn, index) => {
-  btn.addEventListener('click', (event) => {
+  btn.addEventListener("click", (event) => {
     event.preventDefault();
 
     menuHeader.forEach(itemMenu => {
@@ -83,6 +148,13 @@ btnMenu.forEach((btn, index) => {
         itemMenu.classList.remove('active');
         btnMenu.forEach(itemBtn => {
           itemBtn.classList.remove('active')
+        })
+        closeMenu.forEach(itemClose => {
+          itemClose.addEventListener("click", (event) => {
+            event.preventDefault();
+            itemClose.classList.remove('active')
+          })
+          console.log("clicou no botao")
         })
       })
     })
@@ -93,5 +165,34 @@ btnMenu.forEach((btn, index) => {
     
     btn.classList.add('active');
     menuHeader[index].classList.add("active");
+  })
+})
+
+//--------------------------------------------------------------------------------------
+//NAVEGAÇÃO POR TABS COMPLETO MOBILE - MENU
+//--------------------------------------------------------------------------------------
+const btnMenuMobile = document.querySelectorAll('.js-btn-menu-mobile');
+const menuHeaderMobile = document.querySelectorAll('.js-menu-mobile');
+
+btnMenuMobile.forEach((btn, index) => {
+  btn.addEventListener("click", (event) => {
+    event.preventDefault();
+
+    menuHeaderMobile.forEach(itemMenu => {
+      itemMenu.classList.remove('active');
+      itemMenu.addEventListener('mouseleave', () => {        
+        itemMenu.classList.remove('active');
+        btnMenuMobile.forEach(itemBtn => {
+          itemBtn.classList.remove('active')
+        })
+      })
+    })
+
+    btnMenuMobile.forEach(itemBtn => {
+      itemBtn.classList.remove('active')
+    })
+    
+    btn.classList.add('active');
+    menuHeaderMobile[index].classList.add("active");
   })
 })
